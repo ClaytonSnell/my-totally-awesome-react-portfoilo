8b86a0da267eefbf007bd12d09849b1b6599f809
claytontsnell@gmail.com <claytontsnell@gmial.com>
2022-11-16 20:19:36 -0500

    worked with erik, he is a boss

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './pages/About';
+
+function App() {
+  return (
+   <div>
+      <Header />
+      <About />
+      <Footer />
+  </div>
+  );
+}
+
+export default App;
