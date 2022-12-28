36ca69c8c0f2f183e2a1112279888a8443717a38
claytontsnell@gmail.com <claytontsnell@gmial.com>
2022-12-28 14:02:04 -0500

    will this go to the portfolio that is running on gh pages?

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,26 +3,35 @@ import image from '../../src/media/profile.png'
 const styles = {
   card: {
     width:'400px',
-    height:'400px',
-    padding: 25,
+    height:'600px'
+  },
+  aside: {
+    flex: 1,
+    backgroundColor: '#b9c6ae',
+    padding: 20
+  },
+  section: {
+    flex: 3,
+    padding: 20,
+    display: 'flex',
+    flexWrap: 'wrap'
   },
 }
 
 function About() {
     return (
-        <section class="page-section" id="about">
+      <section style={styles.section} class="page-section" id="about" >
+          <h1>About Me</h1>
           <img style={styles.card} src={image} alt= "pic of myself"/>
-        <h1>About Me</h1>
-        <div>
+        <aside style={styles.aside}>
           <p>
-          My name is Clayton Snell. I was born and riased in the Salt Lake Valley of Utah and have lived there for the majority of my life. Though I still own property in Utah, I am currently living with my wife and kids in Maryland. I haveattended a coding bootcamp offered by the University of Utah to change careers and directions in my lifes path.
+          My name is Clayton Snell. I was born and riased in the Salt Lake Valley of Utah and have lived there for the majority of my life. Though my roots are in Utah, I am currently living with my wife and kids in Maryland. I completed a coding bootcamp offered by the University of Utah to change careers and directions in my life path.
           </p>
           <p>
-            Let me holla at ya partner, spit this game up in ya ear for a minitue. Quit comlpaining bout' how you can't stand it becuase you ain't got it. Got what it takes but not enough to get started,
-            I hope you get the message, no its not a test this, just me hollerin' legit they can't arrest me or bust me. I'm still hungry, I want some more stuff.
-            Get fat and watch my whole crew blow up. So get yours and buy my new album peep the game and don't be like calvin. 
+            I am really excited to see how far I can go with coding. I have been taking after course work and will continue to add to my github repositories. I have learned so much in the coding bootcamp which consisted of basic knowledge of
+            HTML, CSS, JavaScript, node.js, learned how to use and build API's, Object Oreiented Programing, Express, SQL, noSQL, ORM, MVC, PWA, React.js, MERN stack, State. Also studied after class Python.js, AWS, 
           </p>
-        </div>
+        </aside>
       </section>
     )
 }
